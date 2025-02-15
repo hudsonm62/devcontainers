322fbc57bc91bb55706211f0ba00b9a872122268
Hudson <hudson@ziroau.com>
2025-02-15 23:08:12 +1100

    Update `hugo-node` test

--- test/hugo-node/test.sh
+++ test/hugo-node/test.sh
@@ -3,7 +3,9 @@ cd $(dirname "$0")
 source test-utils.sh
 
 # Template specific tests
-check "distro" lsb_release -c
+check "distro" cat /etc/os-release
+check "hugo" hugo version
+check "node" node -v
 #check "greeting" [ $(cat /usr/local/etc/greeting.txt | grep hey) ]
 
 # Report result
