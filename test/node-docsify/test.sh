d5a1589cd6f84b0d557584298bb398ea26f3d46d
Hudson <hudson@ziroau.com>
2025-02-16 16:41:01 +1100

    Update test.sh

--- test/node-docsify/test.sh
+++ test/node-docsify/test.sh
@@ -5,7 +5,8 @@ source test-utils.sh
 # Template specific tests
 check "distro" cat /etc/os-release
 check "node" node -v
-check "docsify" docsify-cli -v
+check "npm" npm -v
+check "docsify" docsify -v
 
 # Report result
 reportResults
